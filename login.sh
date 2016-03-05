ece0a634d4bca6858e0b406cda24038daa4ef2b3
Agustin Camino <agustin.camino@gmail.com>
2016-03-05 09:14:34 -0500

    Remove unnecessary scripts

--- login.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://localhost:9292/login \
--d email='ada@stackbuilders.com' \
--d password='secret'
--- signup.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:9292/signup \
--d name='Ada Lovelace' \
--d email='ada@stackbuilders.com' \
--d password='secret' \
--d country_code='+593' \
--d phone_number='992670240'
